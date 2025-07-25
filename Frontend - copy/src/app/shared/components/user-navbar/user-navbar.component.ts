2a3310943a37dbfb26ea924a11c54458cdc46c22
Simran Dung <simran.dung@godigitaltc.com>
2025-07-25 17:28:45 +0530

    changes

--- Frontend - copy/src/app/shared/components/user-navbar/user-navbar.component.ts
+++ Frontend - copy/src/app/shared/components/user-navbar/user-navbar.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-user-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './user-navbar.component.html',
   styleUrls: ['./user-navbar.component.css']
 })
 export class UserNavbarComponent {
+  constructor(private router:Router){}
+
+  logout(){
+    this.router.navigate(['landingPage'])
+
+  }
 
 }
